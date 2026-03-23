b0f8d56827481241381566acca79e61d42c3ca50
istvanandrewthomas <istvanandrewthomas.f@salesianiverona.it>
2026-03-23 15:24:38 +0100

    Add files via upload

---
+++ Verifica matrici e vettori (febbraio).py
@@ -0,0 +1,10 @@
+def main():
+    while True:
+        scelta = int(input("Inserisci quale esercizio vorresti fare (1 o 2): "))
+        if scelta==1:
+            print("Esercizio 1")
+        elif scelta==2:
+            print("Esercizio 2")
+        else:
+            print("Errore!")      
+main()
